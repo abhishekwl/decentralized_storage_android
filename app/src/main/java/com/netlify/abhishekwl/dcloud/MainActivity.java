addd0d7bbbfd552c04d59c9c1015a1ed6f825739
abhishekwl <abhishekwl@protonmail.com>
2019-11-06 21:49:56 +0530

    Initial Commit

---
+++ app/src/main/java/com/netlify/abhishekwl/dcloud/MainActivity.java
@@ -0,0 +1,76 @@
+package com.netlify.abhishekwl.dcloud;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Objects;
+
+import okhttp3.OkHttpClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomAppBar mainBottomAppBar;
+    private FragmentManager fragmentManager;
+    private FloatingActionButton addFab;
+
+    public OkHttpClient okHttpClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initializeComponents();
+        initializeViews();
+    }
+
+    private void initializeViews() {
+        mainBottomAppBar = findViewById(R.id.mainBottomAppBar);
+        setSupportActionBar(mainBottomAppBar);
+        Objects.requireNonNull(getSupportActionBar()).setHomeAsUpIndicator(R.drawable.ic_home_black_24dp);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        mainBottomAppBar.replaceMenu(R.menu.main_menu);
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.mainFrameLayout, new PostsFragment()).commit();
+        addFab = findViewById(R.id.mainAddFab);
+        addFab.setColorFilter(Color.WHITE);
+    }
+
+    private void initializeComponents() {
+        okHttpClient = new OkHttpClient();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.videosMenuItem:
+                fragmentManager.beginTransaction().replace(R.id.mainFrameLayout, new VideosFragment()).commit();
+                break;
+            case R.id.photosMenuItem:
+                fragmentManager.beginTransaction().replace(R.id.mainFrameLayout, new PhotosFragment()).commit();
+                break;
+            case R.id.privateMenuItem:
+                fragmentManager.beginTransaction().replace(R.id.mainFrameLayout, new PrivateFragment()).commit();
+                break;
+            default:
+                fragmentManager.beginTransaction().replace(R.id.mainFrameLayout, new PostsFragment()).commit();
+                break;
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/netlify/abhishekwl/dcloud/PrivateFragment.java
@@ -0,0 +1,30 @@
+package com.netlify.abhishekwl.dcloud;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PrivateFragment extends Fragment {
+
+
+    public PrivateFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_private, container, false);
+    }
+
+}
